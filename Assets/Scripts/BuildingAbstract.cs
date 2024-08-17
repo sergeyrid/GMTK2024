0f2ee036fdb2d5d8cff57e1df960c93a9fe082a8
serchemach <serchemach@gmail.com>
2024-08-17 16:32:14 +0300

    Do stuff

--- Assets/Scripts/BuildingAbstract.cs
+++ Assets/Scripts/BuildingAbstract.cs
@@ -4,14 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class BuildingAbstract : MonoBehaviour
+public abstract class BuildingAbstract : MonoBehaviour
 {
     public float incomeRate = 1;
     public float collectedCurrency = 0;
     public GameObject incomeRateText;
+    public GameObject globalPipe;
+    public GameObject smallPipe;
 
+    private float health = 100;
     private TextMeshProUGUI incomeRateTextMesh;
     private SpriteRenderer buildingSprite;
+
+    void DealDamage(float damage) {
+        health -= damage;
+        if (health <= 0) {
+            Destroy(self);
+        }
+    }
 
     void Awake() {
          buildingSprite = GetComponent<SpriteRenderer>();
@@ -19,6 +29,16 @@ public class BuildingAbstract : MonoBehaviour
     }
 
     void Start() {
+
+        // var smallPipeHeight = globalPipe.GetComponent<SpriteRenderer>().bounds.center.y - buildingSprite.bounds.center.y;
+
+        // Debug.Log("Instantiate");
+        // var smallPipeInstance = Instantiate(smallPipe, new Vector3(0, 0, 0), transform.rotation);
+        // var smallPipeTransform = smallPipeInstance.GetComponent<RectTransform>();
+        // // smallPipeTransform.localPosition = new Vector3(buildingSprite.center.x, buildingSprite.center.y, 0);
+        // smallPipeTransform.anchorMax = new Vector2(buildingSprite.bounds.center.x, buildingSprite.bounds.center.y); 
+        // smallPipeTransform.anchorMin = new Vector2(buildingSprite.bounds.center.x - 1, buildingSprite.bounds.center.y - smallPipeHeight); 
+        
         //  incomeRateTextMesh = incomeRateText.GetComponent<TextMeshPro>();       
         // if(incomeRateTextMesh == null)
         // {
@@ -28,18 +48,21 @@ public class BuildingAbstract : MonoBehaviour
 
     }
 
+    public abstract void UpdateScale(float scale);
+
     void FixedUpdate() {
-        transform.localScale = new Vector3(1, collectedCurrency + 1, 1);
+        UpdateScale(collectedCurrency);
+
         // Debug.Log("SHYYYYyyyyy");
         if(incomeRateTextMesh != null)
         {
-            Debug.Log("SHYYYY");
+            // Debug.Log("SHYYYY");
             // incomeRateTextMesh = incomeRateText.AddComponent<TextMeshProUGUI>();
             incomeRateTextMesh.text = collectedCurrency.ToString();
             incomeRateTextMesh.transform.position = new Vector3(buildingSprite.bounds.max.x, buildingSprite.bounds.max.y + 1, 0);
         }
         else {
-            Debug.Log("!SHYYYY");
+            // Debug.Log("!SHYYYY");
         }
     }
 }
